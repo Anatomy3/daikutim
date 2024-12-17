8fb223ef3625d155759448f441f3e6d1a03589a9
Vigio <linggacrack@gmail.com>
2024-12-17 09:21:13 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
-import '../styles/globals.css'; // Import global CSS di sini
+import '../styles/globals.css';
+import Sidebar from '../components/Sidebar';
+import styles from '../styles/Layout.module.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.layout}>
+      <Sidebar />
+      <main className={styles.mainContent}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
 export default MyApp;
